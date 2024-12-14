1a0d0d9bd42dcd4577148ef42d2582c104d6bd52
Mustafa <mustafa.tambulut@gmail.com>
2024-12-15 02:50:24 +0300

    build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,20 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import terser from "@rollup/plugin-terser";
-import replace from "@rollup/plugin-replace";
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import replace from '@rollup/plugin-replace';
 
 export default {
-  input: "dev/index.html", // Giriş dosyası (projenin ana dosyası)
+  input: 'src/app.js', // Giriş dosyası (projenin ana dosyası)
   output: {
-    file: "dist/bundle.js", // Çıkış dosyası
-    format: "es", // ES module formatında çıktıyı sağlar
+    file: 'dist/bundle.js', // Çıkış dosyası
+    format: 'es', // ES module formatında çıktıyı sağlar
     sourcemap: true,
   },
   plugins: [
     resolve(), // Node modüllerini çözümler
     commonjs(), // CommonJS modüllerini ES modüllerine dönüştürür
     replace({
-      "process.env.NODE_ENV": JSON.stringify("production"),
+      'process.env.NODE_ENV': JSON.stringify('production'),
       preventAssignment: true,
     }),
     terser(), // Kodun minify edilmesini sağlar
