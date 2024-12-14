50898240d19afd7478e6993d1d571113f6172a17
Mustafa <mustafa.tambulut@gmail.com>
2024-12-15 02:35:50 +0300

    Update rollup.config.js - netlify error build

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import terser from '@rollup/plugin-terser';
 import replace from '@rollup/plugin-replace';
 
 export default {
-  input: 'dev/index.html', // Giriş dosyası (projenin ana dosyası)
+  input: 'src/app.js', // Giriş dosyası (projenin ana dosyası)
   output: {
     file: 'dist/bundle.js', // Çıkış dosyası
     format: 'es', // ES module formatında çıktıyı sağlar
